083cef89cd956877177385b40a44f76df5d884cf
Jon <dyercode@users.noreply.github.com>
2020-05-02 02:42:07 -0400

    Got menu compling. doesn't quite work yet. menu is not being displayed.

--- src/Main.elm
+++ src/Main.elm
@@ -2,14 +2,22 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, img, input, li, p, text, ul)
-import Html.Attributes exposing (src)
-import Html.Events exposing (onClick)
+import Html.Attributes as Attrs exposing (src)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode exposing (Decoder, field, int, list, map2, map3, string)
+import Menu
 
 
 type alias Model =
-    { monses : Maybe PokeList, errorMessage : Maybe String }
+    { monses : Maybe PokeList
+    , errorMessage : Maybe String
+    , autoState : Menu.State
+    , query : String
+    , howManyToShow : Int
+    , pokemonList : List PokeListResult
+    , showMenu : Bool
+    }
 
 
 main : Program () Model Msg
@@ -22,18 +30,15 @@ main =
         }
 
 
-defaultList : PokeList
-defaultList =
-    { count = 0
-    , next = ""
-    , results = []
-    }
-
-
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( { monses = Nothing
       , errorMessage = Nothing
+      , autoState = Menu.empty
+      , query = ""
+      , howManyToShow = 0
+      , pokemonList = []
+      , showMenu = False
       }
     , Cmd.none
     )
@@ -42,6 +47,9 @@ init _ =
 type Msg
     = SendHttpRequest
     | GotMons (Result Http.Error PokeList)
+    | SetAutoCompleteState Menu.Msg
+    | SetQuery String
+    | SelectPokemon String
 
 
 buildErrorMessage : Http.Error -> String
@@ -63,22 +71,81 @@ buildErrorMessage httpError =
             message
 
 
+updateConfig : Menu.UpdateConfig Msg PokeListResult
+updateConfig =
+    Menu.updateConfig
+        { toId = .name
+        , onKeyDown =
+            \code maybeId ->
+                if code == 13 then
+                    Maybe.map SelectPokemon maybeId
+
+                else
+                    Nothing
+        , onTooLow = Nothing
+        , onTooHigh = Nothing
+        , onMouseEnter = \_ -> Nothing
+        , onMouseLeave = \_ -> Nothing
+        , onMouseClick = \id -> Just <| SelectPokemon id
+        , separateSelections = False
+        }
+
+
+acceptablePokemon : String -> List PokeListResult -> List PokeListResult
+acceptablePokemon query pokemon =
+    let
+        lowerQuery =
+            String.toLower query
+    in
+    List.filter (String.contains lowerQuery << String.toLower << .name) pokemon
+
+
 update msg model =
     case msg of
         SendHttpRequest ->
             ( model, getPokemons )
 
         GotMons (Ok monses) ->
-            ( { model | monses = Just monses }, Cmd.none )
+            ( { model
+                | monses = Just monses
+                , pokemonList = List.append model.pokemonList monses.results
+              }
+            , Cmd.none
+            )
 
         GotMons (Err errMessage) ->
             ( { model | monses = Nothing, errorMessage = Just (buildErrorMessage errMessage) }, Cmd.none )
+
+        SetAutoCompleteState autoMsg ->
+            let
+                ( newState, maybeMsg ) =
+                    Menu.update updateConfig autoMsg model.howManyToShow model.autoState (acceptablePokemon model.query model.pokemonList)
+            in
+            ( { model | autoState = newState }, Cmd.none )
+
+        SetQuery newQuery ->
+            ( { model | query = newQuery, showMenu = True }
+            , Cmd.none
+            )
+
+        SelectPokemon id ->
+            ( { model
+                | query =
+                    List.filter (\poke -> poke.name == id) model.pokemonList
+                        |> List.head
+                        |> Maybe.withDefault (PokeListResult "unown" "nourl")
+                        |> .name
+                , autoState = Menu.empty
+                , showMenu = False
+              }
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ pokemon 0
+        [ pokemonSelect model
         , button [ onClick SendHttpRequest ]
             [ text "get em all" ]
         , case model.monses of
@@ -146,8 +213,50 @@ pokeListDecoder =
         (field "results" (list pokeListResultDecoder))
 
 
-pokemon id =
+pokemonSelect model =
     div []
         [ img [ src "./media/pokemon/icons/0.png" ] []
-        , input [] []
+        , input
+            [ onInput SetQuery
+            , Attrs.class "autocomplete-input"
+            , Attrs.value model.query
+            ]
+            []
+        , if model.showMenu then
+            viewMenu model
+
+          else
+            Html.div [] []
+        ]
+
+
+viewConfig : Menu.ViewConfig PokeListResult
+viewConfig =
+    let
+        customizedLi keySelected mouseSelected pkmn =
+            { attributes =
+                [ Attrs.classList
+                    [ ( "autocomplete-item", True )
+                    , ( "key-selected", keySelected || mouseSelected )
+                    ]
+                , Attrs.id pkmn.name
+                ]
+            , children = [ Html.text pkmn.name ]
+            }
+    in
+    Menu.viewConfig
+        { toId = .name
+        , ul = [ Attrs.class "autocomplete-list" ]
+        , li = customizedLi
+        }
+
+
+viewMenu : Model -> Html.Html Msg
+viewMenu model =
+    Html.div [ Attrs.class "autocomplete-menu" ]
+        [ Html.map SetAutoCompleteState <|
+            Menu.view viewConfig
+                model.howManyToShow
+                model.autoState
+                (acceptablePokemon model.query model.pokemonList)
         ]
