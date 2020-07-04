326b7d1020f7066b3aecd22f6837d909b74444c7
Jon <dyercode@users.noreply.github.com>
2020-07-04 19:58:35 -0400

    updates, try to fix not launchin

--- src/components/store.ts
+++ src/components/store.ts
@@ -1,15 +1,16 @@
 import { writable, Writable, derived, get } from 'svelte/store';
 import {Config} from "./config";
 import {Pokemon} from '../model/pokemon';
+import {Type} from '../model/type';
 
 export const config: Writable<Config> = writable({useUnknown: false, useShadow: false});
-export const team = writable(Array(6).fill(null))
-export const typedexRaw = writable([])
-export const typedex = derived(typedexRaw, types => {
+export const team: Pokemon[] = writable(Array(6).fill(null))
+export const typedexRaw: any[] = writable([])
+export const typedex = derived(typedexRaw, (types: Type[]) => {
     const c = get(config)
     return types.filter(t => {
         return (t.name !== 'shadow' || c.useShadow) && (t.name !== 'unknown' || c.useUnknown);
     });
 });
 
-export const thinPokedex = writable([])
+export const thinPokedex: Writable<any[]> = writable([])
