eacad45c175887f8f13e50c3e056684d8a35ba48
Jon <dyercode@users.noreply.github.com>
2020-07-23 08:22:16 -0400

    minor test reword.

--- spec/effectiveness.component.spec.ts
+++ spec/effectiveness.component.spec.ts
@@ -7,13 +7,15 @@ import { Effectiveness } from '../src/model/effectiveness';
 describe("Effectiveness", () => {
     // export function teamEffectiveness(attackType: Type, testEffective: Effectiveness, inteam: Pokemon[], typedex: Type[]) {
     describe("teamEffectiveness", () => {
-        it('zero when type not found in dex', () => {
-            expect(teamEffectiveness(new Type("dark"), Effectiveness.Immune, [{name: "darkmon", types: ["psychic"]}], [])).to.equal(0);
-        });
+        describe('given some bad data', () => {
+            it('defaults to zero when type not found in dex', () => {
+                expect(teamEffectiveness(new Type("dark"), Effectiveness.Immune, [{ name: "darkmon", types: ["psychic"] }], [])).to.equal(0);
+            });
 
-        it('defaults to zero when nothing goes in', () => {
-            expect(teamEffectiveness(null, null, null, null)).to.equal(0);
-        });
+            it('defaults to zero when nothing goes in', () => {
+                expect(teamEffectiveness(null, null, null, null)).to.equal(0);
+            });
+        })
     });
 
 });
